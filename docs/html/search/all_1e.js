08ead56182ce1488fe9976bd0102dfc8ac375371
warte67 <121575921+warte67@users.noreply.github.com>
2025-10-06 20:46:44 -0700

    Milestone DisplayObject code feature complete
    Next steps would be IAssetObject interface and the AssetObject handle.

--- docs/html/search/all_1e.js
+++ docs/html/search/all_1e.js
@@ -15,7 +15,7 @@ var searchData=
   ['windowheight_12',['windowHeight',['../structSDOM_1_1Core_1_1CoreConfig.html#a8c437dc7c49ff80c7904ce46fbca14d9',1,'SDOM::Core::CoreConfig']]],
   ['windowwidth_13',['windowWidth',['../structSDOM_1_1Core_1_1CoreConfig.html#acd80478dc5e6f4dc3b4f3c591eafd783',1,'SDOM::Core::CoreConfig']]],
   ['with_20global_20accessor_14',['Example Usage (with global accessor)',['../md_docs_2core__framework.html#autotoc_md19',1,'']]],
-  ['with_20lua_15',['Example: Creating with Lua',['../md_docs_2scripting__and__configuration.html#autotoc_md179',1,'']]],
+  ['with_20lua_15',['Example: Creating with Lua',['../md_docs_2scripting__and__configuration.html#autotoc_md181',1,'']]],
   ['with_20sol2_16',['Example: Lua Integration with Sol2',['../md_docs_2exposing__hooks.html#autotoc_md95',1,'']]],
   ['word_17',['Word',['../SDOM__CLR_8hpp.html#abf4ef44c4c8719bd6d059ca8cdbe0fde',1,'SDOM_CLR.hpp']]],
   ['works_18',['How It Works',['../md_docs_2extensible__factory.html#autotoc_md131',1,'']]],
